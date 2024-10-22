09667067d944a22bb90e2a72efd7c8bbadae360c
marcotriesdev <50000tons@gmail.com>
2024-10-21 18:49:27 -0600

    kaka kulo

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+let variable1 = 0;
+
+console.log("puto el ke lo lea");
+variable1 += 10;
+console.log(variable1);
+
+
+
+document.getElementById('output1').textContent = "puto el ke lo lea: " + variable1.toString();
+variable1 += 20;
+document.getElementById('output2').textContent = "Mentira, jeje:  " + variable1.toString();
